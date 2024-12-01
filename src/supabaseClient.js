3203346d0035b3cc2fbf31c3cfbcb2b20a8be5de
MathieuPesin <mathieu@pesin.info>
2024-12-02 00:33:27 +0100

    Add environment variables and update dependencies

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Remplacez par votre URL Supabase et votre clé API
-const supabaseUrl = 'https://tpulnhufbdxbrhexouij.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwdWxuaHVmYmR4YnJoZXhvdWlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3NDYxODEsImV4cCI6MjA0ODMyMjE4MX0.MU9nmU_5ZgJXDta3cWMkf0qKICkrbph_q4y1V7UXFpI';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
